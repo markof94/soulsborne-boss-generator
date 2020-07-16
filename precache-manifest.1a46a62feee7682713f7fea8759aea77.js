61031cc37b99617bf8aafc058a0c5584c14fc8ee
markof94 <markofajler@gmail.com>
2020-07-16 17:50:36 +0200

    Updates

---
+++ precache-manifest.1a46a62feee7682713f7fea8759aea77.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8900729b12f71b50ab097143f144ba82",
+    "url": "/soulsborne-boss-generator/index.html"
+  },
+  {
+    "revision": "5a4dd697748b4f931e5f",
+    "url": "/soulsborne-boss-generator/static/css/main.871b3c56.chunk.css"
+  },
+  {
+    "revision": "4705368dce3042123576",
+    "url": "/soulsborne-boss-generator/static/js/2.450d1321.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/soulsborne-boss-generator/static/js/2.450d1321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a4dd697748b4f931e5f",
+    "url": "/soulsborne-boss-generator/static/js/main.c85f5ac5.chunk.js"
+  },
+  {
+    "revision": "98a3745dc8342b7e4f49",
+    "url": "/soulsborne-boss-generator/static/js/runtime-main.8c1ac228.js"
+  },
+  {
+    "revision": "22e30314f9091e70a3b8a9d4f64397bb",
+    "url": "/soulsborne-boss-generator/static/media/EBGaramond-Bold.22e30314.ttf"
+  },
+  {
+    "revision": "d6989885f23b82bded861163d8ab44bd",
+    "url": "/soulsborne-boss-generator/static/media/EBGaramond-Regular.d6989885.ttf"
+  },
+  {
+    "revision": "3c8f829071aa912475024288d0f62fc9",
+    "url": "/soulsborne-boss-generator/static/media/bg.3c8f8290.jpg"
+  }
+]);
